51e3af970949b02511ad036e6a35a450a2bf4e84
Milind-TW <milind.kejriwal@thoughtworks.com>
2022-10-19 14:57:40 +0530

    Upgrade eks module

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -9,38 +9,29 @@ data "aws_eks_cluster_auth" "cluster" {
 data "aws_caller_identity" "current" {}
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  host                   = data.aws_eks_cluster.cluster
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+  }
 }
 
 module "k8s-cluster" {
   source                          = "terraform-aws-modules/eks/aws"
-  version                         = "17.24.0"
+  version                         = "18.30.2"
   cluster_name                    = var.cluster_name
   cluster_version                 = "1.22"
-  subnets                         = concat(var.private_subnets, var.public_subnets)
+  subnet_ids                      = var.private_subnets
+  control_plane_subnet_ids        = concat(var.private_subnets, var.public_subnets)
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
-  enable_irsa                     = true
   vpc_id                          = var.vpc_id
   manage_cluster_iam_resources    = true
-  manage_aws_auth                 = true
-  map_roles = [
-    {
-      rolearn  = "${var.role_arn}"
-      username = "gh-runner-user"
-      groups   = ["system:masters"]
-    }
-  ]
-  map_users = [
-    {
-      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/Milind-Admin"
-      username = "Milind-Admin"
-      groups   = ["system:masters"]
-
-    }
-  ]
+  manage_aws_auth_configmap       = true
 
   fargate_profiles = {
     default = {
@@ -87,6 +78,23 @@ module "k8s-cluster" {
     }
 
   }
+
+  aws_auth_roles = [
+    {
+      rolearn  = "${var.role_arn}"
+      username = "gh-runner-user"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/Milind-Admin"
+      username = "Milind-Admin"
+      groups   = ["system:masters"]
+    },
+  ]
+
   tags = {
     Environment = "test"
     GithubRepo  = "terraform-aws-eks"
