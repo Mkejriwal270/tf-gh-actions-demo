93cfa45ba05842e5a2193215a7bd5a4fd0624a1a
Milind-TW <milind.kejriwal@thoughtworks.com>
2022-10-19 12:18:54 +0530

    Add crb, remove custom iam resources, and add artifact workflow

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -15,43 +15,43 @@ provider "kubernetes" {
 }
 
 module "k8s-cluster" {
-    source                          = "terraform-aws-modules/eks/aws"
-    version                         = "17.24.0"
-    cluster_name                    = var.cluster_name
-    cluster_version                 = "1.22"
-    subnets                         = concat(var.private_subnets, var.public_subnets)
-    cluster_endpoint_private_access = true
-    cluster_endpoint_public_access  = true
-    enable_irsa                     = true
-    vpc_id                          = var.vpc_id
-    manage_cluster_iam_resources    = true
-    manage_aws_auth                 = true
-    map_roles                       = [
-        {
-            rolearn  = "${var.role_arn}"
-            username = "gh-runner-user"
-            groups   = ["system:masters"]
-        }
-    ]
-    map_users                       = [
-        {
-            userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/Milind-Admin"
-            username = "Milind-Admin"
-            groups   = ["system:masters"]
-
-        }   
-    ]
-    tags = {
-        Environment = "test"
-        GithubRepo  = "terraform-aws-eks"
-        GithubOrg   = "terraform-aws-modules"
+  source                          = "terraform-aws-modules/eks/aws"
+  version                         = "17.24.0"
+  cluster_name                    = var.cluster_name
+  cluster_version                 = "1.22"
+  subnets                         = concat(var.private_subnets, var.public_subnets)
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+  enable_irsa                     = true
+  vpc_id                          = var.vpc_id
+  manage_cluster_iam_resources    = true
+  manage_aws_auth                 = true
+  map_roles = [
+    {
+      rolearn  = "${var.role_arn}"
+      username = "gh-runner-user"
+      groups   = ["system:masters"]
     }
+  ]
+  map_users = [
+    {
+      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/Milind-Admin"
+      username = "Milind-Admin"
+      groups   = ["system:masters"]
+
+    }
+  ]
+  tags = {
+    Environment = "test"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
 }
 
 resource "aws_eks_fargate_profile" "default" {
   cluster_name           = module.k8s-cluster.cluster_id
   fargate_profile_name   = "default"
-  pod_execution_role_arn = aws_iam_role.fargate-pod-executionrole-actionsdemo.arn
+  pod_execution_role_arn = module.k8s-cluster.fargate_profile_arns[0]
   subnet_ids             = var.private_subnets
 
   selector {
@@ -74,7 +74,7 @@ resource "aws_eks_fargate_profile" "default" {
 resource "aws_eks_fargate_profile" "KubeSystem" {
   cluster_name           = module.k8s-cluster.cluster_id
   fargate_profile_name   = "kube-system"
-  pod_execution_role_arn = aws_iam_role.fargate-pod-executionrole-actionsdemo.arn
+  pod_execution_role_arn = module.k8s-cluster.fargate_profile_arns[0]
   subnet_ids             = var.private_subnets
 
   selector {
@@ -89,7 +89,7 @@ resource "aws_eks_fargate_profile" "KubeSystem" {
 resource "aws_eks_fargate_profile" "CoreDns" {
   cluster_name           = module.k8s-cluster.cluster_id
   fargate_profile_name   = "CoreDNS"
-  pod_execution_role_arn = aws_iam_role.fargate-pod-executionrole-actionsdemo.arn
+  pod_execution_role_arn = module.k8s-cluster.fargate_profile_arns[0]
   subnet_ids             = var.private_subnets
 
   selector {
@@ -100,6 +100,6 @@ resource "aws_eks_fargate_profile" "CoreDns" {
   }
 
   depends_on = [
-      aws_eks_fargate_profile.KubeSystem
+    aws_eks_fargate_profile.KubeSystem
   ]
 }
